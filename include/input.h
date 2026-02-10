0c2fa2068a4630c3271707ebba8f2b38967589b3
Johnny4251 <JohnAngus425@gmail.com>
2026-02-09 21:09:31 -0500

    Update Input

--- include/input.h
+++ include/input.h
@@ -1,12 +1,14 @@
 #pragma once
-
 #include <queue>
 #include <SDL2/SDL.h>
 
 extern std::queue<SDL_Keycode> key_buffer;
+extern bool quit_requested;
 
 void update_key_buffer(SDL_Event& event);
 
 bool has_key();
 SDL_Keycode pop_key();
 void clear_keys();
+
+bool key_down(SDL_Scancode sc);
